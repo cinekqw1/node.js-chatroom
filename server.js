00ed7aec067596cd43eb82a0a40e0e440c0ae8f7
marcin <marcin@AsusDebian.home>
2017-03-24 11:47:36 +0100

    add chat_server.js

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
+var chatServer = require('./lib/chat_server');
 var cache = {};
+
+chat_server.listen(server)
 
 function send404(response) {
   response.writeHead(404,{'Content-Type':'text/plain'});
